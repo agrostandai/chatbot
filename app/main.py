301d4e510e89c5f95d533df1f3e3bfce51480e4a
Keshav Porwal <porwal.keshav.6324@email.com>
2025-08-16 15:27:23 +0530

    tryin' to deploy on render

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from app.routes import main_router
 
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
+
 app = FastAPI(
     title="WhatsApp AI Bot",
     description="FastAPI app to chat and detect crop diseases",
